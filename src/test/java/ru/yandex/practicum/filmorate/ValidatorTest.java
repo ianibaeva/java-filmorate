cddae68cb430c51a25fb5b36a223a0db883274e0
Ada <ianibaeva@yandex.ru>
2023-05-27 02:39:58 +0300

    спринт 9

--- src/test/java/ru/yandex/practicum/filmorate/ValidatorTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/ValidatorTest.java
@@ -1,4 +1,4 @@
-package ru.yandex.practicum.filmorate.validator;
+package ru.yandex.practicum.filmorate;
 
 import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
