0645cd9397805c0efe36c14e6f8257196b0471af
Ada <ianibaeva@yandex.ru>
2023-06-01 03:16:52 +0300

    refactor & feat: sprint 10

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -1,13 +1,20 @@
 package ru.yandex.practicum.filmorate.storage.film;
 
 import ru.yandex.practicum.filmorate.model.Film;
+
 import java.util.List;
 
 public interface FilmStorage {
+
     Film add(Film film);
+
     Film update(Film film);
+
     Film delete(Film film);
+
     Film getById(int id);
+
     List<Film> getAll();
+
     List<Film> getMostPopular(int count);
 }
