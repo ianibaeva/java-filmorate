5610a4cc1e49403768a12c2f2d338cffff2d4a51
Ada <ianibaeva@yandex.ru>
2023-06-17 22:52:44 +0300

    update README.md

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -7,6 +7,7 @@ import ru.yandex.practicum.filmorate.utils.ReleaseDateValidation;
 import javax.validation.constraints.*;
 import java.time.LocalDate;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Data
@@ -24,6 +25,8 @@ public class Film {
     private final int duration;
     private Set<Integer> usersLiked = new HashSet<>();
     private int likes = 0;
+    private MpaRating mpaRating;
+    private List<Genre> genres;
 
 }
 
